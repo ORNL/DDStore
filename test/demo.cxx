8a340fedf5dfca1635b3c5ff2ec7c29bf76fec54
Choi <choij@ornl.gov>
2022-11-14 09:51:06 -0500

    add test

---
+++ test/demo.cxx
@@ -0,0 +1,40 @@
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "dstore.hpp"
+
+int main(int argc, char *argv[])
+{
+    MPI_Init(&argc, &argv);
+
+    MPI_Comm comm = MPI_COMM_WORLD;
+
+    int rank;
+    MPI_Comm_rank(comm, &rank);
+
+    int comm_size;
+    MPI_Comm_size(comm, &comm_size);
+
+    double *buffer;
+    const int N = 4;
+    buffer = (double *)malloc(N * sizeof(double));
+    for (int i = 0; i < N; i++)
+    {
+        buffer[i] = i + 1 + 10 * rank;
+        printf("%d: buffer[%d] = %g\n", rank, i, buffer[i]);
+    }
+
+    DStore ds(comm);
+    ds.add("var", buffer, 2, 2);
+
+    double getbuf[4] = {0.0, 0.0, 0.0, 0.0};
+    int start = (2 * (rank + 1)) % (2 * comm_size) + 1;
+    printf("start: %d\n", start);
+    ds.get("var", start, 1, getbuf);
+
+    printf("%d: %g %g %g %g\n", rank, getbuf[0], getbuf[1], getbuf[2], getbuf[3]);
+
+    MPI_Finalize();
+    return EXIT_SUCCESS;
+}
---
+++ test/demo.py
@@ -0,0 +1,50 @@
+import mpi4py
+
+mpi4py.rc.thread_level = "serialized"
+mpi4py.rc.threads = False
+
+import numpy as np
+from mpi4py import MPI
+import argparse
+import pyddstore as dds
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--num", type=int, help="num. of data (default: %(default)s)", default=1024 * 1024)
+    parser.add_argument("--dim", type=int, help="dim (default: %(default)s)", default=64)
+    parser.add_argument("--nbatch", type=int, help="nbatch (default: %(default)s)", default=32)
+    args = parser.parse_args()
+
+    comm = MPI.COMM_WORLD
+    comm_size = comm.Get_size()
+    rank = comm.Get_rank()
+
+    ddstore = dds.PyDDStore(comm)
+
+    num = args.num
+    dim = args.dim
+    nbatch = args.nbatch
+
+    shape = (num, dim)
+    dtype = np.float64
+    arr = np.ones(shape, dtype=dtype)
+    arr = arr * (rank + 1)
+    arr = arr.reshape(shape)
+    print(rank, "arr", np.mean(arr), arr.nbytes / 1024 / 1024 / 1024, "(GB)")
+    ddstore.add("var", arr)
+
+    comm.Barrier()
+    idx_list = list()
+    buff_list = list()
+    for i in range(nbatch):
+        idx = np.random.randint(num * comm_size)
+        buff = np.zeros(dim, dtype=dtype)
+        ddstore.get("var", buff, idx)
+        idx_list.append(idx)
+        buff_list.append(buff)
+
+    for i, idx in enumerate(idx_list):
+        expected = idx // num + 1
+        assert np.mean(buff_list[i]) == expected, (np.mean(buff_list[i]), expected)
+    print(rank, "done.")

1a77ddf9bf379b97cca4fd69225479721c9d8b65
Choi <choij@ornl.gov>
2022-11-15 16:40:49 -0500

    update test

--- test/test.py
+++ test/test.py
@@ -12,6 +12,62 @@ import os
 import torch
 import torch.distributed as dist
 
+import psutil
+import socket
+
+
+def find_ifname(myaddr):
+    """
+    Find socket ifname for a given ip adress. This is for "GLOO" ddp setup.
+    Usage example:
+        find_ifname("127.0.0.1") will return a network interface name, such as "lo". "lo0", etc.
+    """
+    ipaddr = socket.gethostbyname(myaddr)
+    ifname = None
+    for nic, addrs in psutil.net_if_addrs().items():
+        for addr in addrs:
+            if addr.address == ipaddr:
+                ifname = nic
+                break
+        if ifname is not None:
+            break
+
+    return ifname
+
+
+def parse_slurm_nodelist(nodelist):
+    """
+    Parse SLURM_NODELIST env string to get list of nodes.
+    Usage example:
+        parse_slurm_nodelist(os.environ["SLURM_NODELIST"])
+    Input examples:
+        "or-condo-g04"
+        "or-condo-g[05,07-08,13]"
+        "or-condo-g[05,07-08,13],or-condo-h[01,12]"
+    """
+    nlist = list()
+    for block, _ in re.findall(r"([\w-]+(\[[\d\-,]+\])*)", nodelist):
+        m = re.match(r"^(?P<prefix>[\w\-]+)\[(?P<group>.*)\]", block)
+        if m is None:
+            ## single node
+            nlist.append(block)
+        else:
+            ## multiple nodes
+            g = m.groups()
+            prefix = g[0]
+            for sub in g[1].split(","):
+                if "-" in sub:
+                    start, end = re.match(r"(\d+)-(\d+)", sub).groups()
+                    fmt = "%%0%dd" % (len(start))
+                    for i in range(int(start), int(end) + 1):
+                        node = prefix + fmt % i
+                        nlist.append(node)
+                else:
+                    node = prefix + sub
+                    nlist.append(node)
+
+    return nlist
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -29,10 +85,29 @@ if __name__ == "__main__":
     comm_size = comm.Get_size()
     rank = comm.Get_rank()
 
-    os.environ["MASTER_ADDR"] = "127.0.0.1"
-    os.environ["MASTER_PORT"] = "8889"
+    ## Default setting
+    master_addr = "127.0.0.1"
+    master_port = "8889"
+
+    if os.getenv("LSB_HOSTS") is not None:
+        ## source: https://www.olcf.ornl.gov/wp-content/uploads/2019/12/Scaling-DL-on-Summit.pdf
+        ## The following is Summit specific
+        master_addr = os.environ["LSB_HOSTS"].split()[1]
+    elif os.getenv("LSB_MCPU_HOSTS") is not None:
+        master_addr = os.environ["LSB_MCPU_HOSTS"].split()[2]
+    elif os.getenv("SLURM_NODELIST") is not None:
+        ## The following is CADES specific
+        master_addr = parse_slurm_nodelist(os.environ["SLURM_NODELIST"])[0]
+
+    os.environ["MASTER_ADDR"] = master_addr
+    os.environ["MASTER_PORT"] = master_port
     os.environ["WORLD_SIZE"] = str(comm_size)
     os.environ["RANK"] = str(rank)
+
+    ifname = find_ifname(master_addr)
+    if ifname is not None:
+        os.environ["GLOO_SOCKET_IFNAME"] = ifname
+
     dist.init_process_group(backend="gloo", init_method="env://")
 
     ddstore = dds.PyDDStore(comm)
